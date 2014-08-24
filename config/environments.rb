be0983023818f8d002fbb6e458da1632616fc316
qiaomingwei <281899468@qq.com>
2014-08-24 17:15:32 +0800

    b

--- config/environments.rb
+++
@@ -1,8 +0,0 @@
-ActiveRecord::Base.establish_connection(
-  :adapter => 'mysql2',
-  :host     => 'localhost',
-  :username => 'root',
-  :password => 'root',
-  :database => 'taxfreebazaar',
-  :encoding => 'utf8',
-)
